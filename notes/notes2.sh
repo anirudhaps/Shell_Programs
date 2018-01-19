7e27ce60b9a9d9f77a29372398d4476f9db7ac26
Anirudha P Singh <Anirudha.Singh@cavium.com>
2018-01-19 17:57:37 +0530

    Adding notes file for chap-2

---
+++ notes/notes2.sh
@@ -0,0 +1,5 @@
+# Shell script is a list of system commands stored in a file.
+# It saves a lot of efforts to retype the set of commmands again and 
+# again. Instead, we can run the script.
+# Thereafter, a script becomes a program, a tool that can be modified 
+# further to suit the need.
